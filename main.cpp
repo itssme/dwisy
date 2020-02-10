efd7ca2dd134199423160289ad0cb16ef1c07f05
itssme <itssme3000@gmail.com>
2020-02-11 00:28:56 +0100

    fixed config frame comparison now waiting two seconds for camera adjustment at start

--- main.cpp
+++ main.cpp
@@ -22,20 +22,27 @@ int main() {
     Mat frame;
     Mat config_frame;
     Mat compared_frame;
+    const int renew_config_interval{(int) config.fps};
+    int renew_config_frame{renew_config_interval};
     Size size(config.width/config.height * MOVEMENT_DETECTION_IMAGE_SIZE, MOVEMENT_DETECTION_IMAGE_SIZE);
 
     std::cout << "Waiting for config frame" << std::endl;
 
     { // find first frame for later comparison
         bool config_frame_found = false;
+        int warm_up{(int) config.fps * 2}; // wait for first camera adjustment for 2 seconds
         while (not config_frame_found) {
             video_stream >> frame;
 
             if (not frame.empty()) {
-                resize(frame, config_frame, size);
-                cvtColor(frame, frame, COLOR_BGR2GRAY);
-                GaussianBlur(frame, frame, Size_<int>(21, 21), 0);
-                config_frame_found = true;
+                if (not warm_up) {
+                    resize(frame, config_frame, size);
+                    cvtColor(config_frame, config_frame, COLOR_BGR2GRAY);
+                    GaussianBlur(config_frame, config_frame, Size_<int>(21, 21), 0);
+                    config_frame_found = true;
+                } else {
+                    warm_up--;
+                }
             }
         }
     }
@@ -53,16 +60,24 @@ int main() {
         cvtColor(frame, frame, COLOR_BGR2GRAY);
         GaussianBlur(frame, frame, Size_<int>(21, 21), 0);
 
-        absdiff(frame, config_frame, compared_frame);
-        //threshold(compared_frame, compared_frame, 20, 255, THRESH_BINARY);
-        //dilate(compared_frame, compared_frame, Mat());
+        if (not renew_config_frame) {
+            renew_config_frame = renew_config_interval;
+            config_frame = frame;
+        } else {
+            renew_config_frame -= 1;
+        }
 
-        resize(frame, frame, size);
-        imshow("UsbImage", frame);
+        absdiff(frame, config_frame, compared_frame);
+        threshold(compared_frame, compared_frame, 20, 255, THRESH_BINARY);
+        dilate(compared_frame, compared_frame, Mat());
+
+        imshow("Frame", frame);
+        imshow("config_frame", config_frame);
+        imshow("Compared", compared_frame);
+
         char c = (char) waitKey(25);
         if (c == 27)
             break;
-
     }
 
     video_stream.release();
